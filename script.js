a08c8bc85f54541edd3c28d3a8369378ddbcfbd1
Divyarabekka <137371881+Divyarabekka@users.noreply.github.com>
2023-07-24 11:39:44 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+
+setTimeout(myFunction, 1000);
+function myFunction() {
+  document.getElementById("demo").innerHTML = "6";  
+}
+
+setTimeout(myFunction1, 2000);
+function myFunction1() {
+  document.getElementById("demo").innerHTML = "5";  
+}
+
+setTimeout(myFunction2, 3000);
+function myFunction2() {
+  document.getElementById("demo").innerHTML = "4";  
+}
+
+setTimeout(myFunction3, 4000);
+function myFunction3() {
+  document.getElementById("demo").innerHTML = "3";  
+}
+
+setTimeout(myFunction4, 5000);
+function myFunction4() {
+  document.getElementById("demo").innerHTML = "2";  
+}
+
+setTimeout(myFunction5, 6000);
+function myFunction5() {
+  document.getElementById("demo").innerHTML = "1";  
+}
+
+setTimeout(myFunction6, 7000);
+function myFunction6() {
+  document.getElementById("demo").innerHTML = "Happy independence day";  
+}
